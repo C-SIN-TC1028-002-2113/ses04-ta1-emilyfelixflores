b2eb09f1c5fbe01ffab5c95d1180d14eb04333e2
emilyfelixflores <A01742374@itesm.mx>
2021-08-21 23:04:32 +0000

    Ejercicios Tarea 4

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+
+    if peso>0 and altura>0:
+        if (peso/altura**2)<20:
+            print("PESO BAJO")
+        elif (peso/altura**2)<=20 or (peso/altura**2)<25:
+            print("NORMAL")
+        elif (peso/altura**2)<=25 or (peso/altura**2)<30:
+            print("SOBREPESO")
+        elif (peso/altura**2)<=30 or (peso/altura**2)<40:
+            print("OBESIDAD")
+        elif (peso/altura**2)>=40:
+            print("OBESIDAD MORBIDA")
+    else:
+        print("Revisa tus datos, alguno de ellos es erróneo.")
+    
+    
+
 
 if __name__=='__main__':
     main()
